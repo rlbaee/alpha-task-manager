a84ba60b20b1e743a86b6a91831e7f5cd880fe94
Gaabriel Ponomarjov <pgaabriel@icloud.com>
2026-08-03 14:55:40 +0300

    Add macOS support

--- forge.config.js
+++ forge.config.js
@@ -1,57 +1,81 @@
-const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const { FusesPlugin } = require("@electron-forge/plugin-fuses");
+const { FuseV1Options, FuseVersion } = require("@electron/fuses");
 
 module.exports = {
   publishers: [
     {
-      name: '@electron-forge/publisher-github',
+      name: "@electron-forge/publisher-github",
       config: {
         repository: {
-          owner: 'your-github-username',
-          name: 'alpha-task-manager',
+          owner: "rlbaee",
+          name: "alpha-task-manager",
         },
         prerelease: false,
       },
     },
   ],
+
   packagerConfig: {
     asar: true,
+
+    // App icons
+    icon: "icon",
   },
+
   rebuildConfig: {},
+
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "alpha_task_manager",
+      },
+    },
+
+    {
+      name: "@electron-forge/maker-dmg",
+      platforms: ["darwin"],
+      config: {
+        format: "ULFO",
+      },
+    },
+
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
+
+    {
+      name: "@electron-forge/maker-deb",
       config: {},
     },
+
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-rpm",
       config: {},
     },
   ],
+
   plugins: [
     {
-      name: '@electron-forge/plugin-auto-unpack-natives',
+      name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
+
     new FusesPlugin({
       version: FuseVersion.V1,
+
       [FuseV1Options.RunAsNode]: false,
+
       [FuseV1Options.EnableCookieEncryption]: true,
+
       [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+
       [FuseV1Options.EnableNodeCliInspectArguments]: false,
+
       [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
-    
   ],
 };
